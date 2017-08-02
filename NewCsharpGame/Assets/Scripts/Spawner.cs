450ec34452fb2aad3a0313504e48b4cfa5e48ada
MoLeighJackson <moleighjackson@gmail.com>
2017-08-02 19:40:10 -0400

    Added Files

---
+++ NewCsharpGame/Assets/Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] prefabs;
+    public float delay = 2.0f;
+    public bool active = true;
+    public Vector2 delayRange = new Vector2(1, 2);
+
+	
+	void Start () {
+        ResetDelay();
+        StartCoroutine(ObstacleGenerator());
+	}
+
+    IEnumerator ObstacleGenerator()
+    {
+        yield return new WaitForSeconds(delay);
+
+        if(active)
+        {
+            var newTransform = transform;
+
+            Instantiate(prefabs[UnityEngine.Random.Range(0, prefabs.Length)], newTransform.position, Quaternion.identity);
+            ResetDelay();
+
+        }
+        StartCoroutine(ObstacleGenerator());
+    }
+
+    void ResetDelay()
+    {
+        delay = UnityEngine.Random.Range(delayRange.x, delayRange.y);
+    }
+
+}
